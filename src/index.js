16581d1393a26360ad1dc571723a2f7e8a6f3499
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-23 12:29:39 +0200

    Added useEffect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
 
-appRoot.render(
-    <h1>I changed somethings</h1>
+function App (){
+    let [rates, setRates] = React.useState({});
+    
+    function fetchData() {
+        axios.get('https://api.exchangerate.host/latest?base=UAH')
+        .then(response => {
+            setRates(response.data.rates);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+
+    React.useEffect(fetchData, []);
+
+    return Object.keys(rates).map(e => 
+        <h1>One hryvna is equal to { rates[e].toFixed(2) } of {e}</h1>
+    );
+}
+
+
+appRoot.render( [
+    <App />,
+    <App />
+]
 );
+
