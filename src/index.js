99dcc754e9c101b99798c579d3f2fa663093e865
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-08-23 11:28:56 +0200

    Used fetching without useEffect

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
 
 appRoot.render(
-    <h1>My refresher app</h1>
+    <h1>I changed somethings</h1>
 );
